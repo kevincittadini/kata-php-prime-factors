dcdf635fbae9febd5fb020eb4afaf22fa3f79cc1
Kevin Cittadini <cittadini.kevin@gmail.com>
2018-10-26 12:42:08 +0200

    Break numbers in prime factors.

--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -4,5 +4,23 @@ namespace PrimeFactors;
 
 class PrimeFactors
 {
-    
+
+    /**
+     * @param int $number
+     *
+     * @return array
+     */
+    public static function generate(int $number): array
+    {
+        $primeFactors = [];
+
+        for ($canditate = 2; $canditate <= $number; $canditate++) {
+            while ($number % $canditate === 0) {
+                $primeFactors[] = $canditate;
+                $number /= $canditate;
+            }
+        }
+
+        return $primeFactors;
+    }
 }
--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -3,8 +3,48 @@
 namespace PrimeFactors\Test;
 
 use PHPUnit\Framework\TestCase;
+use PrimeFactors\PrimeFactors;
 
 class PrimeFactorsTest extends TestCase
 {
 
+    /**
+     * @test
+     */
+    public function case0()
+    {
+        $this->assertEquals([], PrimeFactors::generate(1));
+    }
+
+    /**
+     * @test
+     */
+    public function case2()
+    {
+        $this->assertEquals([2], PrimeFactors::generate(2));
+    }
+
+    /**
+     * @test
+     */
+    public function case3()
+    {
+        $this->assertEquals([2, 2], PrimeFactors::generate(4));
+    }
+
+    /**
+     * @test
+     */
+    public function case4()
+    {
+        $this->assertEquals([2, 3], PrimeFactors::generate(6));
+    }
+
+    /**
+     * @test
+     */
+    public function case5()
+    {
+        $this->assertEquals([2, 2, 3, 3, 5], PrimeFactors::generate(180));
+    }
 }
